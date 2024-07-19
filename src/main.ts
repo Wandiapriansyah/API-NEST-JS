ca3f0012b96df12b8aa77af5da34dc9f63a087ed
Wandi Apriansyah <wandiapriansyah.tkj@gmail.com>
2024-07-19 16:39:34 +0700

    add swagger di main ts

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,22 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(
     AppModule,
   );
+  const config = new DocumentBuilder()
+  .setTitle('Cats Example')
+  .setDescription('The cats API description')
+  .setVersion('1.0')
+  .addTag('cats')
+  .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
   app.useGlobalPipes(new ValidationPipe())
-  app.setGlobalPrefix('api/user'); //tambah prefik wajib
+  // app.setGlobalPrefix('api/user'); //tambah prefik wajib
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('hbs');
